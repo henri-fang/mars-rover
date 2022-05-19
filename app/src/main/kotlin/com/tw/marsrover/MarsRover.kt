17c1414fc38dbc17946dc44eacdb230af6a98eac
Fang Ming <fangming1987200@gmail.com>
2022-05-19 20:49:44 +0800

    Refactor

--- app/src/main/kotlin/com/tw/marsrover/MarsRover.kt
+++ app/src/main/kotlin/com/tw/marsrover/MarsRover.kt
@@ -1,37 +1,37 @@
 package com.tw.marsrover
 
-class MarsRover(private val x: Int, private val y: Int, private val orientation: Direction) {
+class MarsRover {
 
-    fun position(): Position {
-        return Position(x, y, orientation)
+    fun position(position: Position): Position {
+        return position
     }
 
-    fun execute(command: Command): Position =
+    fun execute(position: Position, command: Command): Position =
         when (command) {
-            Command.L -> turnLeft()
-            Command.R -> turnRight()
-            Command.M -> move()
+            Command.L -> turnLeft(position)
+            Command.R -> turnRight(position)
+            Command.M -> move(position)
         }
 
-    private fun move(): Position =
-        when (orientation) {
-            Direction.N -> Position(x, y + 1, orientation)
-            Direction.S -> Position(x, y - 1, orientation)
-            Direction.E -> Position(x + 1, y, orientation)
-            Direction.W -> Position(x - 1, y, orientation)
+    private fun move(position: Position): Position =
+        when (position.orientation) {
+            Direction.N -> Position(position.x, position.y + 1, position.orientation)
+            Direction.S -> Position(position.x, position.y - 1, position.orientation)
+            Direction.E -> Position(position.x + 1, position.y, position.orientation)
+            Direction.W -> Position(position.x - 1, position.y, position.orientation)
         }
 
-    private fun turnRight(): Position = when (orientation) {
-        Direction.N -> Position(x, y, Direction.E)
-        Direction.E -> Position(x, y, Direction.S)
-        Direction.S -> Position(x, y, Direction.W)
-        Direction.W -> Position(x, y, Direction.N)
+    private fun turnRight(position: Position): Position = when (position.orientation) {
+        Direction.N -> Position(position.x, position.y, Direction.E)
+        Direction.E -> Position(position.x, position.y, Direction.S)
+        Direction.S -> Position(position.x, position.y, Direction.W)
+        Direction.W -> Position(position.x, position.y, Direction.N)
     }
 
-    private fun turnLeft(): Position = when (orientation) {
-        Direction.N -> Position(x, y, Direction.W)
-        Direction.E -> Position(x, y, Direction.N)
-        Direction.S -> Position(x, y, Direction.E)
-        Direction.W -> Position(x, y, Direction.S)
+    private fun turnLeft(position: Position): Position = when (position.orientation) {
+        Direction.N -> Position(position.x, position.y, Direction.W)
+        Direction.E -> Position(position.x, position.y, Direction.N)
+        Direction.S -> Position(position.x, position.y, Direction.E)
+        Direction.W -> Position(position.x, position.y, Direction.S)
     }
 }
--- app/src/test/kotlin/com/tw/marsrover/MarsRoverSpec.kt
+++ app/src/test/kotlin/com/tw/marsrover/MarsRoverSpec.kt
@@ -4,37 +4,38 @@ import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 
 class MarsRoverSpec : ShouldSpec({
+
     should("return the initial position if the mars rover does not move") {
-        MarsRover(0, 0, Direction.N).position() shouldBe Position(0, 0, Direction.N)
+        MarsRover().position(Position(0, 0, Direction.N)) shouldBe Position(0, 0, Direction.N)
     }
 
     should("return the right-hand side of mars rover's orientation when it receive R") {
-        MarsRover(0, 0, Direction.N).execute(Command.R) shouldBe Position(0, 0, Direction.E)
-        MarsRover(0, 0, Direction.E).execute(Command.R) shouldBe Position(0, 0, Direction.S)
-        MarsRover(0, 0, Direction.S).execute(Command.R) shouldBe Position(0, 0, Direction.W)
-        MarsRover(0, 0, Direction.W).execute(Command.R) shouldBe Position(0, 0, Direction.N)
+        MarsRover().execute(Position(0, 0, Direction.N), Command.R) shouldBe Position(0, 0, Direction.E)
+        MarsRover().execute(Position(0, 0, Direction.E), Command.R) shouldBe Position(0, 0, Direction.S)
+        MarsRover().execute(Position(0, 0, Direction.S), Command.R) shouldBe Position(0, 0, Direction.W)
+        MarsRover().execute(Position(0, 0, Direction.W), Command.R) shouldBe Position(0, 0, Direction.N)
     }
 
     should("return the left-hand side of mars rover's orientation when it receive L") {
-        MarsRover(0, 0, Direction.N).execute(Command.L) shouldBe Position(0, 0, Direction.W)
-        MarsRover(0, 0, Direction.E).execute(Command.L) shouldBe Position(0, 0, Direction.N)
-        MarsRover(0, 0, Direction.S).execute(Command.L) shouldBe Position(0, 0, Direction.E)
-        MarsRover(0, 0, Direction.W).execute(Command.L) shouldBe Position(0, 0, Direction.S)
+        MarsRover().execute(Position(0, 0, Direction.N), Command.L) shouldBe Position(0, 0, Direction.W)
+        MarsRover().execute(Position(0, 0, Direction.E), Command.L) shouldBe Position(0, 0, Direction.N)
+        MarsRover().execute(Position(0, 0, Direction.S), Command.L) shouldBe Position(0, 0, Direction.E)
+        MarsRover().execute(Position(0, 0, Direction.W), Command.L) shouldBe Position(0, 0, Direction.S)
     }
 
     should("return position 1 step further on Y co-ordinates when it receive M given mars rover's orientation is North") {
-        MarsRover(0, 0, Direction.N).execute(Command.M) shouldBe Position(0, 1, Direction.N)
+        MarsRover().execute(Position(0, 0, Direction.N), Command.M) shouldBe Position(0, 1, Direction.N)
     }
 
     should("return position 1 step closer on Y co-ordinates when it receive M given mars rover's orientation is South") {
-        MarsRover(0, 1, Direction.S).execute(Command.M) shouldBe Position(0, 0, Direction.S)
+        MarsRover().execute(Position(0, 1, Direction.S), Command.M) shouldBe Position(0, 0, Direction.S)
     }
 
     should("return position 1 step further on X co-ordinates when it receive M given mars rover's orientation is East") {
-        MarsRover(0, 0, Direction.E).execute(Command.M) shouldBe Position(1, 0, Direction.E)
+        MarsRover().execute(Position(0, 0, Direction.E), Command.M) shouldBe Position(1, 0, Direction.E)
     }
 
     should("return position 1 step closer on X co-ordinates when it receive M given mars rover's orientation is West") {
-        MarsRover(1, 0, Direction.W).execute(Command.M) shouldBe Position(0, 0, Direction.W)
+        MarsRover().execute(Position(1, 0, Direction.W), Command.M) shouldBe Position(0, 0, Direction.W)
     }
 })
